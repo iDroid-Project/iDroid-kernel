e59466904aaa2dcf16a843c7d40c8d601fabe75d
Erfan Abdi <erfangplus@gmail.com>
2016-09-06 15:44:34 +0430

    Some Other fixes

--- drivers/dma/apple-cdma.c
+++ drivers/dma/apple-cdma.c
@@ -50,6 +50,9 @@ struct cdma_segment_tag
 #define CDMA_DISABLE(x)			(0x8 + ((x)*0x4))
 #define CDMA_STATUS(x)			(0x10 + ((x)*0x4))
 
+#define DMA 					0x87000000
+#define DMA_AES 				0x800000
+
 #define CDMA_CHAN(x)			(0x1000*((x)+1))
 #define CDMA_CSTATUS(x)			(CDMA_CHAN(x) + 0x0)
 #define CDMA_CCONFIG(x)			(CDMA_CHAN(x) + 0x4)
@@ -403,7 +406,7 @@ int cdma_cancel(u32 _channel)
 
 	wait_for_completion(&cdma_completion);
 	state = cdma_state; // TODO: somewhat hacky.
-	cstate = &state->channels[_channel];
+                                                       	cstate = &state->channels[_channel];
 
 	if(_channel > state->num_channels)
 		return -ENOENT;
@@ -436,6 +439,23 @@ int cdma_cancel(u32 _channel)
 }
 EXPORT_SYMBOL_GPL(cdma_cancel);
 
+int cdma_wait_timeout(u32 _channel,int timeout)
+{
+	struct cdma_state *state;
+	struct cdma_channel_state *cstate;
+
+	wait_for_completion_timeout(&cdma_completion,timeout);
+	state = cdma_state; // TODO: somewhat hacky.
+	cstate = &state->channels[_channel];
+
+	if(_channel > state->num_channels)
+		return -ENOENT;
+
+	wait_for_completion_timeout(&cstate->completion,timeout);
+	return 0;
+}
+EXPORT_SYMBOL_GPL(cdma_wait_timeout);
+
 int cdma_wait(u32 _channel)
 {
 	struct cdma_state *state;
@@ -452,6 +472,7 @@ int cdma_wait(u32 _channel)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(cdma_wait);
+
 
 int cdma_aes(u32 _channel, struct cdma_aes *_aes)
 {
@@ -559,7 +580,7 @@ int cdma_aes(u32 _channel, struct cdma_aes *_aes)
 			break;
 		}
 		break;
-	
+
 	default:
 		dev_err(&state->dev->dev, "invalid AES key 0x%012x.\n", type & 0xFFF);
 		cdma_activate(state, _channel, status);
@@ -571,6 +592,258 @@ int cdma_aes(u32 _channel, struct cdma_aes *_aes)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(cdma_aes);
+
+#define SET_REG(x,y)	writel(y,x)
+
+typedef struct dmaAES_CRYPTO {
+	uint32_t *unkn0;
+	uint32_t unkn1;
+	uint32_t *buffer;
+	uint32_t size;
+	uint32_t unkn4;
+	uint32_t unkn5;
+	uint32_t unkn6;
+	uint32_t unkn7;
+	uint32_t unkn8;
+	uint32_t unkn9;
+	uint32_t unkn10;
+	uint32_t unkn11;
+	uint32_t unkn12;
+	uint32_t unkn13;
+	uint32_t unkn14;
+	uint32_t unkn15;
+} __attribute__((packed)) dmaAES_CRYPTO;
+
+static dmaAES_CRYPTO *aes_crypto = NULL; // first one is not used. lol.
+
+#define DMA_SETTINGS 0x4
+#define DMA_TXRX_REGISTER 0x8
+#define DMA_SIZE 0xC
+int aes_hw_crypto_operation(uint32_t _arg0, uint32_t _channel, uint32_t *_buffer, uint32_t _arg3, uint32_t _size, uint32_t _arg5, uint32_t _arg6, uint32_t _arg7) {
+	uint32_t unkn0;
+	uint32_t value = 0;
+	uint32_t channel_reg = _channel << 12;
+
+	SET_REG(DMA + channel_reg, 2);
+
+	aes_crypto[_channel].buffer = _buffer;
+	aes_crypto[_channel].unkn0 = &aes_crypto[_channel].unkn8;
+	aes_crypto[_channel].unkn1 = (_arg7 ? 0x30103 : 0x103);
+	aes_crypto[_channel].size = _size;
+	aes_crypto[_channel].unkn9 = 0;
+
+	if(_arg0) {
+		switch(_arg5)
+		{
+			case 1:
+				value |= (0 << 2);
+				break;
+			case 2:
+				value |= (1 << 2);
+				break;
+			case 4:
+				value |= (2 << 2);
+				break;
+			default:
+				return -1;
+		}
+
+		switch(_arg6)
+		{
+			case 1:
+				value |= (0 << 4);
+				break;
+			case 2:
+				value |= (1 << 4);
+				break;
+			case 4:
+				value |= (2 << 4);
+				break;
+			case 8:
+				value |= (3 << 4);
+				break;
+			case 16:
+				value |= (4 << 4);
+				break;
+			case 32:
+				value |= (5 << 4);
+				break;
+			default:
+				return -1;
+		}
+		if(_arg0 == 1)
+			value |= 2;
+		unkn0 = 0;
+		SET_REG(DMA + channel_reg + DMA_SETTINGS, value);
+		SET_REG(DMA + channel_reg + DMA_TXRX_REGISTER, _arg3);
+	} else {
+		unkn0 = 128;
+	}
+	//DataCacheOperation(1, (uint32_t)&aes_crypto[_channel], 0x20);
+	SET_REG(DMA + channel_reg + DMA_SIZE, _size);
+	SET_REG(DMA + channel_reg + 0x14, (uint32_t)&aes_crypto[_channel]);
+	SET_REG(DMA + channel_reg, (unkn0 | (_arg7 << 8) | 0x1C0000));
+	return 0;
+}
+
+//todo
+int aes_hw_crypto_cmd(uint32_t _encrypt, uint32_t *_inBuf, uint32_t *_outBuf, uint32_t _size, uint32_t _type, uint32_t* _key, uint32_t *_iv) {
+
+
+	struct cdma_state *state;
+		struct cdma_channel_state *cstate;
+		u32 type, keytype;
+		wait_for_completion(&cdma_completion);
+		state = cdma_state; // TODO: somewhat hacky.
+
+		int value = 0;
+
+	//clock_gate_switch(0x14, ON);
+	cdma_activate(state, 1, 1);
+	cdma_activate(state, 2, 1);
+
+	uint32_t channel = 1;
+	uint32_t channel_reg = channel << 12;
+	value = (channel & 0xFF) << 8;
+
+	if(_size & 0xF)
+		panic("aes_hw_crypto_cmd: bad arguments\r\n");
+
+	if(_encrypt & 0xF0) {
+		if((_encrypt & 0xF0) != 0x10)
+			panic("aes_hw_crypto_cmd: bad arguments\r\n");
+		value |= (1 << 17);
+	}
+
+	if(_encrypt & 0xF) {
+		if((_encrypt & 0xF) != 1)
+			panic("aes_hw_crypto_cmd: bad arguments\r\n");
+	} else {
+		value |= (1 << 16);
+	}
+
+	if(_iv) {
+		writel(_iv[0], 0x87801010);
+		writel(_iv[1], 0x87801014);
+		writel(_iv[2], 0x87801018);
+		writel(_iv[3], 0x8780101C);
+	} else {
+		writel(0, 0x87801010);
+		writel(0, 0x87801014);
+		writel(0, 0x87801018);
+		writel(0, 0x8780101C);
+	}
+
+
+	keytype = (type >> 28) & 0xF;
+
+	uint32_t key_shift = 0;
+	uint32_t key_set = 0;
+	if ((_type & 0xFFF) == 0) {
+		switch(keytype)
+		{
+			case 2:	// AES 256
+				value |= (2 << 18);
+				break;
+
+			case 1:	// AES 192
+				value |= (1 << 18);
+				break;
+
+			case 0:	// AES 128
+				value |= (0 << 18);
+				break;
+
+			default:// Fail
+				panic("aes_hw_crypto_cmd: bad arguments\r\n");
+		}
+		switch(keytype)
+				{
+				case 2: // AES-256
+					writel(_key[7], state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 7));
+					writel(_key[6], state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 6));
+
+				case 1: // AES-192
+					writel(_key[5], state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 5));
+					writel(_key[4], state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 4));
+
+				default: // AES-128
+					writel(_key[3], state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 3));
+					writel(_key[2], state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 2));
+					writel(_key[1], state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 1));
+					writel(_key[0], state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 0));
+					break;
+				}
+		key_set = 1;
+	} else {
+		if ((_type & 0xFFF) == 512) {
+			key_shift = 1; // still broken
+		} else if ((_type & 0xFFF) == 513) {
+			key_shift = 0;
+		} else {
+			key_shift = 2; // wrong I guess but never used
+		}
+		// Key deactivated?
+		if(readl(DMA + DMA_AES) & (1 << key_shift))
+			return -1;
+	}
+
+	if(key_shift)
+		value |= 1 << 19;
+
+	value |= (key_set << 20) | (key_shift << 21);
+	writel(value, state->aes_regs + CDMA_AES(cstate->aes_channel));
+
+	if(aes_hw_crypto_operation(0, 1, _inBuf, 0, _size, 0, 0, 1) || aes_hw_crypto_operation(0, 2, _outBuf, 0, _size, 0, 0, 0))
+		panic("aes_hw_crypto_cmd: bad arguments\r\n");
+
+	//DataCacheOperation(1, (uint32_t)_inBuf, _size);
+	//DataCacheOperation(3, (uint32_t)_outBuf, _size);
+	writel(readl(DMA + (1 << 12)) | 1, DMA + (1 << 12));
+	writel(readl(DMA + (2 << 12)) | 1, DMA + (2 << 12));
+
+	cdma_wait(1);
+	cdma_wait(2);
+
+	if(key_set)
+	{
+		writel(0, state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 0));
+		writel(0, state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 1));
+		writel(0, state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 2));
+		writel(0, state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 3));
+		writel(0, state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 4));
+		writel(0, state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 5));
+		writel(0, state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 6));
+		writel(0, state->aes_regs + CDMA_AES_KEY(cstate->aes_channel, 7));
+	}
+
+	cdma_activate(state, 1, 0);
+	cdma_activate(state, 2, 0);
+
+	return 0;
+}
+
+int aes_crypto_cmd(uint32_t _encrypt, void *_inBuf, void *_outBuf, uint32_t _size, uint32_t _type, void *_key, void *_iv) {
+	if(_size & 0xF || (!(_type & 0xFFF) && (_key == NULL)) || (_encrypt & 0xF) > 1) {
+		printk("aes_crypto_cmd: bad arguments\r\n");
+		return -1;
+	}
+
+	aes_crypto = kmalloc(sizeof(dmaAES_CRYPTO) * 3, GFP_DMA32);
+	if (!aes_crypto) {
+		printk("aes_crypto_cmd: out of memory\r\n");
+		return -1;
+	}
+
+	if(aes_hw_crypto_cmd(_encrypt, (uint32_t*)_inBuf, (uint32_t*)_outBuf, _size, _type, (uint32_t*)_key, (uint32_t*)_iv)) {
+		kfree(aes_crypto);
+		return -1;
+	}
+
+	kfree(aes_crypto);
+	return 0;
+}
+EXPORT_SYMBOL_GPL(aes_crypto_cmd);
 
 static irqreturn_t cdma_irq_handler(int _irq, void *_token)
 {
@@ -806,3 +1079,4 @@ MODULE_DESCRIPTION("Apple CDMA");
 MODULE_AUTHOR("Richard Ian Taylor");
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("platform:apple-cdma");
+
